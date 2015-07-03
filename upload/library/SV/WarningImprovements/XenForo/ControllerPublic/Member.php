a344c2f9f4ff241863c84d5557a00b10e9c3bbe3
Xon <tazipper1@hotmail.com>
2015-07-03 22:44:40 +0800

    Show list of warning actions on the front end, with permissions for access
    Bugfix

--- upload/library/SV/WarningImprovements/XenForo/ControllerPublic/Member.php
+++ upload/library/SV/WarningImprovements/XenForo/ControllerPublic/Member.php
@@ -6,7 +6,53 @@ class SV_WarningImprovements_XenForo_ControllerPublic_Member extends XFCP_SV_War
     {
         SV_WarningImprovements_Globals::$warning_user_id = $this->_input->filterSingle('user_id', XenForo_Input::UINT);
 
-        return parent::actionMember();
+        $response = parent::actionMember();
+
+        if ($response instanceof XenForo_ControllerResponse_View)
+        {
+            $warningActionModel = $this->_getWarningActionModel();
+            $user = $response->params['user'];
+            if ($warningActionModel->canViewUserWarningActions($user))
+            {
+                $showAll = $warningActionModel->canViewNonSummaryUserWarningActions($user);
+                $showDiscouraged = $warningActionModel->canViewDiscouragedWarningActions($user);
+                $response->params['warningActionsCount'] = $warningActionModel->countWarningActionsByUser($user['user_id'], $showAll, $showDiscouraged);
+                $response->params['canViewWarningActions'] = true;
+            }
+        }
+
+        return $response;
+    }
+
+    public function actionWarningActions()
+    {
+        $userId = $this->_input->filterSingle('user_id', XenForo_Input::UINT);
+        $user = $this->getHelper('UserProfile')->assertUserProfileValidAndViewable($userId);
+
+        $warningActionModel = $this->_getWarningActionModel();
+        if (!$warningActionModel->canViewUserWarningActions($user))
+        {
+            return $this->responseNoPermission();
+        }
+
+        $showAll = $warningActionModel->canViewNonSummaryUserWarningActions($user);
+        $showDiscouraged = $warningActionModel->canViewDiscouragedWarningActions($user);
+
+        $warningActions = $warningActionModel->getWarningActionsByUser($user['user_id'], $showAll, $showDiscouraged);
+        if (!$warningActions)
+        {
+            return $this->responseMessage(new XenForo_Phrase('sv_this_user_has_no_warning_actions'));
+        }
+
+        $warningActions = $warningActionModel->prepareWarningActions($warningActions);
+
+        $viewParams = array
+        (
+            'user' => $user,
+            'warningActions' => $warningActions
+        );
+        return $this->responseView('SV_WarningImprovements_ViewPublic_Member_WarningActions', 'sv_member_warning_actions', $viewParams);
+
     }
 
     public function actionWarn()
@@ -29,5 +75,10 @@ class SV_WarningImprovements_XenForo_ControllerPublic_Member extends XFCP_SV_War
         SV_WarningImprovements_Globals::$warning_user_id = $this->_input->filterSingle('user_id', XenForo_Input::UINT);
 
         return parent::actionCard();
+    }
+
+    protected function _getWarningActionModel()
+    {
+        return $this->getModelFromCache('XenForo_Model_UserChangeTemp');
     }
 }
--- upload/library/SV/WarningImprovements/XenForo/Model/UserChangeTemp.php
+++ upload/library/SV/WarningImprovements/XenForo/Model/UserChangeTemp.php
@@ -2,4 +2,208 @@
 
 class SV_WarningImprovements_XenForo_Model_UserChangeTemp extends XFCP_SV_WarningImprovements_XenForo_Model_UserChangeTemp
 {
+    public function countWarningActionsByUser($userId, $showAll = false, $showDiscouraged = false)
+    {
+        $sql = '';
+        if (!$showDiscouraged)
+        {
+            $sql .= " and action_type <> 'field' and action_modifier <> 'is_discouraged' ";
+        }
+
+        if ($showAll)
+        {
+            $select = 'user_change_temp_id';
+        }
+        else
+        {
+            $select = 'distinct action_type, new_value';
+        }
+
+        return $this->_getDb()->fetchOne('
+            SELECT COUNT(' . $select . ')
+            FROM xf_user_change_temp
+            WHERE user_id = ? ' . $sql . '
+        ', $userId);
+    }
+
+    public function getWarningActionsByUser($userId, $showAll = false, $showDiscouraged = false)
+    {
+        $sql = '';
+        if (!$showDiscouraged)
+        {
+            $sql .= " and action_type <> 'field' and action_modifier <> 'is_discouraged' ";
+        }
+
+        if ($showAll)
+        {
+            $where = 'user_id = ? ' . $sql;
+        }
+        else
+        {
+            $where = ' user_change_temp_id in
+                (
+                    select max(user_change_temp_id)
+                    from xf_user_change_temp
+                    where user_id = ? ' . $sql . '
+                    group by action_type, new_value
+                )
+            ';
+        }
+        return $this->fetchAllKeyed('
+            SELECT xf_user_change_temp.*, user_change_temp_id as warning_action_id
+            FROM xf_user_change_temp
+            WHERE ' . $where . '
+            ORDER BY expiry_date DESC
+        ', 'warning_action_id', array($userId));
+    }
+
+    public function prepareWarningActions($warningActions)
+    {
+        $actions = array();
+        foreach($warningActions as $warningAction)
+        {
+            $warning_action = $this->prepareWarningAction($warningAction);
+            if (!empty($warning_action))
+            {
+                $actions[] = $warning_action;
+            }
+        }
+        return $actions;
+    }
+
+    public function prepareWarningAction(array $warningAction)
+    {
+        $warning_type = array
+        (
+            'new_value' => $warningAction['new_value'],
+            'old_value' => $warningAction['old_value'],
+        );
+
+        switch ($warningAction['action_type'])
+        {
+            case 'groups':
+                $warning_type['field'] = 'secondary_group_ids';
+                break;
+            case 'field':
+                if ($warningAction['action_modifier'] == 'is_discouraged')
+                {
+                    $warning_type['field'] = $warningAction['action_modifier'];
+                    break;
+                }
+                return null;
+            default:
+                return null;
+        }
+
+        $warning_type = $this->_getHelper()->prepareField($warning_type);
+
+        switch ($warningAction['action_type'])
+        {
+            case 'groups':
+                static $added_to_user_groups_phrase = null;
+                if (empty($added_to_user_groups_phrase))
+                {
+                    $added_to_user_groups_phrase = new XenForo_Phrase('sv_warning_action_added_to_user_groups');
+                }
+                $warningAction['name'] = $added_to_user_groups_phrase;
+                $warningAction['result'] = $warning_type['new_value'];
+                break;
+            case 'field':
+                // This means "discouraged", but it could really be any field on the account forcebly changed.
+                static $discouraged_phrase = null;
+                if (empty($discouraged_phrase))
+                {
+                    $discouraged_phrase = new XenForo_Phrase('discouraged');
+                }
+                $warningAction['name'] = $discouraged_phrase;
+                $warningAction['result'] = $warning_type['new_value'];
+                break;
+            default:
+                // unknown
+                return null;
+        }
+
+        // round up to the nearest hour
+        $expiry_date = $warningAction['expiry_date'];
+        $prev_hour = $expiry_date - ($expiry_date % 1800);
+        $warningAction['expiry_date'] = $prev_hour + 1800;
+
+        return $warningAction;
+    }
+
+    public function canViewDiscouragedWarningActions(array $user, &$errorPhraseKey = '', array $viewingUser = null)
+    {
+        $this->standardizeViewingUserReference($viewingUser);
+
+        if (empty($viewingUser['user_id']))
+        {
+            return false;
+        }
+
+        $show_discouraged_warning_actions = XenForo_Application::getOptions()->sv_show_discouraged_warning_actions;
+        switch($show_discouraged_warning_actions)
+        {
+            case 0: // Admin/Mod/User
+                return $viewingUser['is_admin'] || $viewingUser['is_moderator'] || ($user['user_id'] == $user['user_id']);
+            case 1: // Admin/Mod
+                return $viewingUser['is_admin'] || $viewingUser['is_moderator'];
+            case 2: // Admin
+                return $viewingUser['is_admin'];
+            case 3:
+            default: // None
+                return false;
+        }
+    }
+
+    public function canViewNonSummaryUserWarningActions(array $user, &$errorPhraseKey = '', array $viewingUser = null)
+    {
+        $this->standardizeViewingUserReference($viewingUser);
+
+        if (empty($viewingUser['user_id']))
+        {
+            return false;
+        }
+
+        return XenForo_Permission::hasPermission($viewingUser['permissions'], 'general', 'sv_showAllWarningActions');
+    }
+
+    public function canViewUserWarningActions(array $user, &$errorPhraseKey = '', array $viewingUser = null)
+    {
+        $this->standardizeViewingUserReference($viewingUser);
+
+        if (empty($viewingUser['user_id']))
+        {
+            return false;
+        }
+
+        if (($user['user_id'] == $user['user_id']))
+        {
+            return true;
+        }
+
+        return XenForo_Permission::hasPermission($viewingUser['permissions'], 'general', 'sv_viewWarningActions');
+    }
+
+    public function canEditUserWarningActions(array $user, &$errorPhraseKey = '', array $viewingUser = null)
+    {
+        $this->standardizeViewingUserReference($viewingUser);
+
+        if (empty($viewingUser['user_id']))
+        {
+            return false;
+        }
+
+        return XenForo_Permission::hasPermission($viewingUser['permissions'], 'general', 'sv_editWarningActions');
+    }
+
+    protected $_helperObject = null;
+    protected function _getHelper()
+    {
+        if ($this->_helperObject === null)
+        {
+            $class = XenForo_Application::resolveDynamicClass('XenForo_Helper_UserChangeLog');
+            $this->_helperObject = new $class();
+        }
+        return $this->_helperObject;
+    }
 }
