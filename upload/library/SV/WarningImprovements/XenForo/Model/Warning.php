7d9f5f4be746ab586df7afe77100d9a934ea020c
Xon <tazipper1@hotmail.com>
2016-12-22 16:09:01 +0800

    Allow middle-click to be useful in AdminCP

--- upload/library/SV/WarningImprovements/XenForo/Model/Warning.php
+++ upload/library/SV/WarningImprovements/XenForo/Model/Warning.php
@@ -482,12 +482,15 @@ class SV_WarningImprovements_XenForo_Model_Warning extends XFCP_SV_WarningImprov
                 {
                     $node['parent'] = '#';
                 }
+                $node['state']['opened'] = 1;
+                $node['a_attr']["href"] = XenForo_Link::buildAdminLink('warnings/category-edit', array(), array('warning_category_id' => $warningItem['warning_category_id']));
             }
             elseif ($this->isWarningDefinition($warningItem))
             {
                 $node['id'] = 'd'.$warningItem['warning_definition_id'];
                 $node['type'] = 'definition';
                 $node['parent'] = 'c'.$warningItem['sv_warning_category_id'];
+                $node['a_attr']["href"] = XenForo_Link::buildAdminLink('warnings/edit', $warningItem);
             }
 
             $node['text'] = $warningItem['title'];
