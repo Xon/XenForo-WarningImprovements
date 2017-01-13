64cf7aa21d6293ad5095907b39ff56bfad7ed0bb
Jeremy P <jrahmy+github@gmail.com>
2017-01-13 00:33:31 -0800

    Warning Action DW: Verification warning category ID

--- upload/library/SV/WarningImprovements/XenForo/DataWriter/WarningAction.php
+++ upload/library/SV/WarningImprovements/XenForo/DataWriter/WarningAction.php
@@ -19,8 +19,9 @@ class SV_WarningImprovements_XenForo_DataWriter_WarningAction extends XFCP_SV_Wa
             'default' => 0
         );
         $fields['xf_warning_action']['sv_warning_category_id'] = array(
-            'type'    => self::TYPE_UINT,
-            'default' => 0
+            'type'         => self::TYPE_UINT,
+            'default'      => 0,
+            'verification' => ['$this', '_verifyWarningCategoryId']
         );
 
         return $fields;
@@ -32,5 +33,29 @@ class SV_WarningImprovements_XenForo_DataWriter_WarningAction extends XFCP_SV_Wa
         {
             $this->bulkSet(SV_WarningImprovements_Globals::$warningActionInput);
         }
+    }
+
+    protected function _verifyWarningCategoryId($warningCategoryId)
+    {
+        if (empty($warningCategoryId))
+        {
+            return false;
+        }
+
+        $warningCategory = $this->_getWarningModel()->getWarningCategoryById(
+            $warningCategoryId
+        );
+
+        if (!empty($warningCategory))
+        {
+            return true;
+        }
+
+        $this->error(
+            new XenForo_Phrase('sv_please_enter_valid_warning_category_id'),
+            'sv_warning_category_id'
+        );
+
+        return false;
     }
 }
