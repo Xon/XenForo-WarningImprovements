94c98926b314c7b4b042b9166890de6b42f98eb5
Xon <tazipper1@hotmail.com>
2017-07-09 18:22:24 +0800

    Allow warning fixup task to always run from the installer

--- upload/library/SV/WarningImprovements/Installer.php
+++ upload/library/SV/WarningImprovements/Installer.php
@@ -201,10 +201,7 @@ class SV_WarningImprovements_Installer
         }
 */
 
-        if ($version && $version < 1050700)
-        {
-            XenForo_Application::defer('SV_WarningImprovements_Deferred_WarningActionFixup1050700', array());
-        }
+        XenForo_Application::defer('SV_WarningImprovements_Deferred_WarningActionFixup1050700', array());
 
         return true;
     }
